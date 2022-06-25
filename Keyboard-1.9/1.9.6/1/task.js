af64a96de1969492e4c0d38d7c27fbb419252443
Vladislav <76947615+never91veup@users.noreply.github.com>
2022-06-25 11:55:44 +0300

    added ninth keyboard

---
+++ Keyboard-1.9/1.9.6/1/task.js
@@ -0,0 +1,11 @@
+function A() {}
+function B() {}
+
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert( a instanceof B ); // true
+
+//  по логике instanceof, именно prototype в действительности определяет тип,
+// а не функция-конструктор
